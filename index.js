50b88f4041439f6100052e46e749279a26177699
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-11-02 14:14:52 +0000

    complete all 5 questions

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
-//1
-function areBothSame(){
-  
+/*
+define two parameters num1 and num2 
+if statement to compare the two parameters values
+if num1 = num2 return true
+else return false
+*/
+function areBothSame(num1, num2){
+  if (num1 == num2){
+      return true;
+  }else{
+      return false
+  }
 }
+console.log(areBothSame("5", 2))
 
-//2
-function areBothEqual(){
-  
+/*
+define two parameters num1 and num2 
+if statement to compare the two parameters values AND datatype
+if num1 = num2 return true
+else return false
+*/
+function areBothEqual(num1, num2){
+  if (num1 === num2){
+      return true;
+  }else{
+      return false
+  }
 }
 
 //3
-function areAllFourEqual(){
-  
+function areAllFourEqual(num1, num2, num3, num4){
+  if (num1 === num2 && num2 === num3 && num3 === num4){
+      return true;
+  }else{
+      return false;
+  }
 }
 
 
 //4
-function areAllFourSame(){
-  
+function areAllFourSame(num1, num2, num3, num4){
+  if (num1 == num2 && num2 == num3 && num3 == num4){
+      return true;
+  }else{
+      return false;
+  }
 }
 
 //5
-function areAllFourSameOrEqual(){
-  
+function areAllFourSameOrEqual(num1, num2, num3, num4){
+  if (num1 === num2 && num2 === num3 && num3 === num4){
+      return "equal";
+  }else if(num1 == num2 && num2 == num3 && num3 == num4){
+      return "same";
+  }else{
+      return null;
+  }
 }
 
+console.log(areAllFourSameOrEqual(true, false, 1, "1"));
 
