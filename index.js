e05903f097bc5eddda325cc5e84f3fda2f7f06a9
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-11-02 14:20:45 +0000

    simplify first two questions into one liners

--- index.js
+++ index.js
@@ -5,26 +5,18 @@ if num1 = num2 return true
 else return false
 */
 function areBothSame(num1, num2){
-  if (num1 == num2){
-      return true;
-  }else{
-      return false
-  }
+  return num1 == num2;
 }
 console.log(areBothSame("5", 2))
 
 /*
 define two parameters num1 and num2 
-if statement to compare the two parameters values AND datatype
-if num1 = num2 return true
+compare the two parameters values AND datatype
+if num1 === num2 return true
 else return false
 */
 function areBothEqual(num1, num2){
-  if (num1 === num2){
-      return true;
-  }else{
-      return false
-  }
+  return num1 === num2;
 }
 
 //3
@@ -39,7 +31,7 @@ function areAllFourEqual(num1, num2, num3, num4){
 
 //4
 function areAllFourSame(num1, num2, num3, num4){
-  if (num1 == num2 && num2 == num3 && num3 == num4){
+  if(num1 == num2 && num2 == num3 && num3 == num4){
       return true;
   }else{
       return false;
